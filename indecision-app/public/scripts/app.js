1ecdbbf84d1af39ec84574cff4e802579a12eddb
James Waller <james.waller1984@gmail.com>
2018-04-07 18:48:49 +1000

    comments removed

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -61,27 +61,3 @@ var VisibilityToggle = function (_React$Component) {
 }(React.Component);
 
 ReactDOM.render(React.createElement(VisibilityToggle, null), document.getElementById('app'));
-
-// let toggle = false;
-
-// const toggleVisibilty = () => {
-//         toggle = !toggle;
-//         render();
-// };
-
-// const appRoot = document.getElementById('app');
-
-// const render = () => {
-//     const template = (
-//         <div>
-//             <h1>Toggle Visibility</h1>
-//             <button onClick={toggleVisibilty}>{toggle ? 'Hide details' : 'Show details'}</button>
-//             {toggle && (
-//                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-//         )} 
-//         </div>
-//     );
-//     ReactDOM.render(template, appRoot);
-// }
-
-// render();
--- indecision-app/src/playground/build-it-visible.js
+++ indecision-app/src/playground/build-it-visible.js
@@ -27,27 +27,3 @@ class VisibilityToggle extends React.Component {
 }
 
 ReactDOM.render(<VisibilityToggle />, document.getElementById('app'));
-
-// let toggle = false;
-
-// const toggleVisibilty = () => {
-//         toggle = !toggle;
-//         render();
-// };
-
-// const appRoot = document.getElementById('app');
-
-// const render = () => {
-//     const template = (
-//         <div>
-//             <h1>Toggle Visibility</h1>
-//             <button onClick={toggleVisibilty}>{toggle ? 'Hide details' : 'Show details'}</button>
-//             {toggle && (
-//                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-//         )} 
-//         </div>
-//     );
-//     ReactDOM.render(template, appRoot);
-// }
-
-// render();
