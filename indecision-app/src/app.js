d80b697229472883a83dadb99966e8b4d3787dac
James Waller <james.waller1984@gmail.com>
2018-04-25 12:06:28 +1000

    localStorage now used for storing and saving data.

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -10,10 +10,23 @@ class IndecisionApp extends React.Component {
         }
     }
     componentDidMount() {
-        console.log('Fetch data')
+        try {
+            const json = localStorage.getItem('options');
+            const options = JSON.parse(json);
+
+            if (options) {
+                this.setState(() => ({ options }))
+            }
+        } catch (e) {
+            // Do nothing!
+        }
     }
     componentDidUpdate(prevProps, prevState) {
-        console.log('Save data')
+        if (prevState.options.length !== this.state.options.length) {
+            const json = JSON.stringify(this.state.options)
+            localStorage.setItem('options', json)
+            console.log('Save data')
+        }
     }
     componentWillUnmount() {
         console.log('componentWillUnmount')
@@ -122,6 +135,7 @@ const Options = (props) => {
         <div>
             <Option />
             <button onClick={props.handleDeleteOptions}>{props.buttonText}</button>
+            {props.options.length === 0 && <p>Please add an option to get started!</p>}
             {
                 props.options.map((option) => <Option 
                     key={option} 
@@ -186,6 +200,10 @@ class AddOption extends React.Component {
         const error = this.props.handleAddOption(option);
 
         this.setState(() => ({ error }))
+
+        if (!error) {
+            e.target.elements.option.value = '';
+        }
     }
     render() {
         return (
