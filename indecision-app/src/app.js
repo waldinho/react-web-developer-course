f26b3f346628bf227cf8f3e2fdf85cf8243afa34
James Waller <james.waller1984@gmail.com>
2018-07-14 08:23:09 +1000

    local storage now persisting data in counter

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -6,7 +6,7 @@ class IndecisionApp extends React.Component {
         this.handleAddOption = this.handleAddOption.bind(this)
         this.handleDeleteOption = this.handleDeleteOption.bind(this)
         this.state = {
-            options: [props.options]
+            options: []
         }
     }
     componentDidMount() {
@@ -78,10 +78,6 @@ class IndecisionApp extends React.Component {
             </div>
         )
     }
-}
-
-IndecisionApp.defaultProps = {
-    options: []
 }
 
 const Header = (props) => {
